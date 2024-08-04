1b3d5222c4b1d0b56a1b9aaab2939de7085b5836
zzw <zzw827@163.com>
2024-08-05 00:42:50 +0800

    调试测试

--- HookAu.lua
+++ HookAu.lua
@@ -1,4 +1,8 @@
 
+---@type ns
+local ns = select(2, ...) 
+
+
 
 print("HookAu")
 
@@ -20,33 +24,70 @@ _G["gAuctionatorBuyDialogMixinSelf"] = nil
 -- Auctionator.AH.PlaceAuctionBid(self.buyInfo.index, self.auctionData.stackPrice) 
 
 
--- local function myFunction()
---     print("这个函数每30秒执行一次。")
---     QueryAuctionItems("白", 1, 10, 0, nil, nil, false, false, nil)
--- end
+local auDoItemsing = false
+local function myAUDoItems()
+    auDoItemsing = true 
+    for index = 1, GetNumAuctionItems("list") do
+        local auctionInfo = { GetAuctionItemInfo("list", index) }
+        local itemLink = GetAuctionItemLink("list", index)
+        local timeLeft = GetAuctionItemTimeLeft("list", index)
+        local entry = {
+            info = auctionInfo,
+            itemLink = itemLink,
+            timeLeft = timeLeft - 1, --Offset to match Retail time parameters
+            index = index,
+        }
 
--- -- 创建一个定时器，每10秒钟调用一次myFunction
--- local myTicker = C_Timer.NewTicker(30, myFunction)
+        ----- 统计当前情况 
+        -- print("Auctionator.AH.DumpAuctions(view",index,auctionInfo,itemLink,timeLeft) 
+        local stackPrice = auctionInfo[Auctionator.Constants.AuctionItemInfo.Buyout]
+        local count = auctionInfo[Auctionator.Constants.AuctionItemInfo.Quantity] 
+        local seller = auctionInfo[Auctionator.Constants.AuctionItemInfo.Owner]
+        local avgGold = stackPrice/count/10000
+        if avgGold>0 and avgGold < 0.08 then
+            print(GetServerTime(),index,seller,itemLink,stackPrice,count,avgGold) 
+            -- Auctionator.AH.PlaceAuctionBid(index, stackPrice)
+            -- PlaceAuctionBid("list", index, stackPrice)
+            -- PlaceAuctionBid("list", 2, 8056)
+            auDoItemsing = false
+            return 
+        end
+        --print(GetServerTime(),itemLink,stackPrice,count,)
 
--- print(GetNumAuctionItems("list"))
--- for index = 1, GetNumAuctionItems(view) do
+    end 
+    auDoItemsing = false
+end
+-- SetBindingMacro("CTRL-X", "MyAddonMacro")
+
+
+
+
+
+local function myAUTicker()
+    canQuery,canQueryAll = CanSendAuctionQuery()
+    print( GetServerTime(), "myAUTicker  canQuery:", canQuery,auDoItemsing) 
+    if canQuery and not auDoItemsing then 
+        --SortAuctionSetSort("list", "unitprice")
+        --QueryAuctionItems("奥杜尔的圣物", nil, nil , 0, nil, nil, false, true, nil)
+        --C_Timer.After(3, myAUDoItems)
+    end 
     
---     local auctionInfo = { GetAuctionItemInfo(view, index) }
---     local itemLink = GetAuctionItemLink(view, index)
---     local timeLeft = GetAuctionItemTimeLeft(view, index)
---     local entry = {
---       info = auctionInfo,
---       itemLink = itemLink,
---       timeLeft = timeLeft - 1, --Offset to match Retail time parameters
---       index = index,
---     }
+end
 
---     ----- 统计当前情况 
---     -- print("Auctionator.AH.DumpAuctions(view",index,auctionInfo,itemLink,timeLeft) 
---     local stackPrice = auctionInfo[Auctionator.Constants.AuctionItemInfo.Buyout]
---     local count = auctionInfo[Auctionator.Constants.AuctionItemInfo.Quantity]
---     -- print(itemLink,stackPrice,count,stackPrice/count/10000)
-
---     -----
---     table.insert(auctions, entry)
---   end
+local auEventFrame = CreateFrame("Frame", "MyauEventFrame")
+auEventFrame:RegisterEvent("AUCTION_HOUSE_SHOW")
+auEventFrame:RegisterEvent("AUCTION_HOUSE_CLOSED")
+local myTicker = nil
+auEventFrame:SetScript("OnEvent", function(self, event, ...)
+    if event == "AUCTION_HOUSE_SHOW" then
+        print(GetServerTime(), "拍卖行已打开。",myTicker)
+        myTicker = C_Timer.NewTicker(10, myAUTicker)
+        --print(ns.ThreeDimensionsCode)
+        ns.ThreeDimensionsCode:send("hhhhh")
+    elseif event == "AUCTION_HOUSE_CLOSED" then
+        print("拍卖行已关闭。",myTicker) 
+        if myTicker and not myTicker:IsCancelled() then 
+            myTicker:Cancel()
+        end 
+    end
+end) 
